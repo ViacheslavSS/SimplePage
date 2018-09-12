26718ba465cf8bab5aa74545b4eb7e9d84646a37
Vyacheslav <kuldur196932@gmail.com>
2018-09-12 08:39:22 +0300

    Add my test method

---
+++ src/main/java/com/Pages/HomePage.java
@@ -0,0 +1,62 @@
+package com.Pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.simpleTests.NewTest;
+
+
+public class HomePage extends NewTest {
+	@FindBy(css = "#sidebar a")
+	List<WebElement> sedeBar;
+	@FindBy(css = "#main-message > h1")
+	WebElement message;
+	@FindBy(className = "post")
+	List<WebElement> post;
+	@FindBy(css = "div >.post p:nth-of-type(1)")
+	WebElement p1;
+	@FindBy(css = "div >.post p:nth-of-type(2)")
+	WebElement p2;
+	
+
+	public String isNovigateSuccess() {
+
+		System.out.println("======  text is   =======" + message.getText().toString());
+
+		return message.getText().toString();
+	}
+
+	public void selectMenuItem(int index) {
+		System.out.println("======size is =======" + sedeBar.size());
+		System.out.println(sedeBar.get(index).getText().toString());
+		sedeBar.get(index).click();
+
+	}
+	
+	public String selectPostByTytle(int index){
+		System.out.println("=======Post size is========"+post.size());
+		
+		return post.get(index).getText().toString();
+		
+		
+	}
+
+	public String getParagraphByIndex(int index) {
+		if(index==1){
+			System.out.println("=====p1 text is======"+p1.getText().toString());
+			return p1.getText().toString();
+			
+		}
+		else if(index==2){
+			System.out.println("=====p1 text is======"+p2.getText().toString());
+			return p2.getText().toString();
+			
+		}
+		return "";
+		
+		
+	}
+
+}
---
+++ src/test/java/com/simpleTests/NewTest.java
@@ -0,0 +1,80 @@
+package com.simpleTests;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.Pages.HomePage;
+
+public class NewTest {
+	protected static WebDriver driver;
+
+	@Test
+	public void testSelectMenuItems() {
+		String message = "Your file was not found";
+
+		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+		driver.manage().timeouts().pageLoadTimeout(3, TimeUnit.SECONDS);
+
+		homePage.selectMenuItem(0);
+		assertEquals(homePage.isNovigateSuccess(), message);
+		driver.navigate().back();
+		homePage.selectMenuItem(1);
+		assertEquals(homePage.isNovigateSuccess(), message);
+		driver.navigate().back();
+		homePage.selectMenuItem(2);
+		assertEquals(homePage.isNovigateSuccess(), message);
+		driver.navigate().back();
+		homePage.selectMenuItem(3);
+		assertEquals(homePage.isNovigateSuccess(), message);
+		driver.navigate().back();
+
+	}
+
+	@Test
+	public void testPostTitle() {
+		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+		assertTrue(homePage.selectPostByTytle(0).contains("Post Title"));
+		assertTrue(homePage.selectPostByTytle(1).contains("Post Title"));
+
+	}
+
+	@Test
+	public void TestPostContent() {
+		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+		String paragrh1 = homePage.getParagraphByIndex(1);
+		assertTrue(paragrh1.contains("Slava"));
+		String paragraf2 = homePage.getParagraphByIndex(2);
+		assertTrue(paragraf2.contains("Starusev"));
+
+	}
+	
+	@Test
+	public void Mytest() {
+		
+System.out.println("Test git");
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/com/chromeWebdriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///C:/Users/Slava/Documents/Html%20pages/testPage1.html");
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		 driver.quit();
+	}
+
+}
